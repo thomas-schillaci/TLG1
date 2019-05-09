f3c3da6379bb9fdf79a337b32d156a87b565788c
aymericvarasse <taille94Crayon>
2019-05-09 17:34:10 +0200

    Shortcut 1

---
+++ src/com/twolazyguys/sprites/Shortcut.java
@@ -0,0 +1,17 @@
+package com.twolazyguys.sprites;
+
+import com.twolazyguys.Main;
+import com.twolazyguys.util.ColorSpritesheet;
+
+public class Shortcut extends Sprite {
+
+    private Sprite[] sprites;
+
+    private static ColorSpritesheet spritesheet = new ColorSpritesheet(1, 27, "abc");
+
+    public Shortcut() {
+
+    }
+
+
+}
