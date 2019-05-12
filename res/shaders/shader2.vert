fde453809acf03c8394c4abf86f9caadb2abd69f
Thomas Schillaci <thomas.schillaci@gmail.com>
2019-05-12 22:09:04 +0200

    shaders and terminal improvements

--- res/shaders/shader2.vert
+++ res/shaders/shader2.vert
@@ -14,10 +14,16 @@ varying vec3 v_bright;
 varying vec3 v_dark;
 varying float v_random;
 
+float cosh(float x) {
+    return (exp(x)+exp(-x))/2;
+}
+
 void main() {
-    float r = 1.0 - pow((abs(2.0 * gl_Vertex.x / 640 - 1.0) - 1.0), 1.0) * pow((abs(2.0 * gl_Vertex.y / 360 - 1.0) - 1.0), 1.0);
+    float a =  0.0020577467;
+    float b = 0.0036582163;
+    float r = 1.0 - (2.0 - cosh(a*gl_Vertex.x)) * (2.0 - cosh(a*gl_Vertex.y));
+    r=1.0;
     mat4 distortion;
-    r = 1.0;
     distortion[0] = vec4(r, 0.0, 0.0, 0.0);
     distortion[1] = vec4(0.0, r, 0.0, 0.0);
     distortion[2] = vec4(0.0, 0.0, 1.0, 0.0);
@@ -25,7 +31,7 @@ void main() {
 
     gl_Position = distortion * projection * view * model * gl_Vertex;
 
-    v_intensity = min(1.0, a_intensity + mod(gl_Position.x*128, 2)/20);
+    v_intensity = min(1.0, a_intensity + mod(int(gl_Position.x*160), 2)/20);
     v_bright = u_bright;
     v_dark = u_dark;
 }
--- src/com/twolazyguys/gamestates/Game.java
+++ src/com/twolazyguys/gamestates/Game.java
@@ -2,7 +2,6 @@ package com.twolazyguys.gamestates;
 
 import com.twolazyguys.Main;
 import com.twolazyguys.entities.Colormap;
-import com.twolazyguys.events.AttackEvent;
 import com.twolazyguys.events.GameTickEvent;
 import com.twolazyguys.sprites.LoadingBar;
 import com.twolazyguys.sprites.Terminal;
@@ -19,50 +18,52 @@ import static org.lwjgl.opengl.GL11.glClearColor;
 
 public class Game extends GameState implements Listener {
 
-	private Terminal terminal = new Terminal();
+    private boolean[] keys = new boolean[65536];
 
-	private boolean[] keys = new boolean[65536];
+    public final static Color BRIGHT = new Color(92, 92, 48);
+    public final static Color DARK = new Color(53, 53, 28);
 
-	public final static Color BRIGHT = new Color(92, 92, 48);
-	public final static Color DARK = new Color(53, 53, 28);
+    private final int X_PIXELS = 320, Y_PIXELS = 180;
 
-	private final int X_PIXELS = 256, Y_PIXELS = 144;
+    private Colormap colormap;
 
-	private Colormap colormap;
+    public Game() {
+        glClearColor(BRIGHT.r, BRIGHT.g, BRIGHT.b, BRIGHT.a);
 
-	public Game() {
-		glClearColor(BRIGHT.r, BRIGHT.g, BRIGHT.b, BRIGHT.a);
+        Terminal terminal = new Terminal();
+        LoadingBar lb = new LoadingBar();
+        colormap = new Colormap(X_PIXELS, Y_PIXELS);
 
-		Main.addListener(terminal);
+        Main.addListener(terminal);
+        Main.addListener(lb);
+        Main.addListener(colormap);
 
-		colormap = new Colormap(X_PIXELS, Y_PIXELS);
-		colormap.addSprite(terminal);
 
-		entities.add(colormap);
-		Main.addListener(colormap);
-		LoadingBar lb = new LoadingBar();
-		colormap.addSprite(lb);
-		Main.addListener(lb);
-	}
+        entities.add(colormap);
 
-	@EventHandler
-	public void onKeyboardInputEvent(KeyboardInputEvent e) {
-		keys[e.getKey()] = e.getAction() != GLFW_RELEASE;
-		
-		if (e.getAction() == GLFW_PRESS) {}
-	}
+        colormap.addSprite(terminal);
+        colormap.addSprite(lb);
+    }
 
-	public boolean isKeyDown(int keycode) {
-		return keys[keycode];
-	}
+    @EventHandler
+    public void onKeyboardInputEvent(KeyboardInputEvent e) {
+        keys[e.getKey()] = e.getAction() != GLFW_RELEASE;
 
-	@Override
-	public void update() {
-		GLFW.glfwSetWindowTitle(Main.window, getClass().getName() + " - " + Main.fps + "fps");
-		Main.callEvent(new GameTickEvent());
-	}
+        if (e.getAction() == GLFW_PRESS) {
+        }
+    }
 
-	public Colormap getColormap() {
-		return colormap;
-	}
+    public boolean isKeyDown(int keycode) {
+        return keys[keycode];
+    }
+
+    @Override
+    public void update() {
+        GLFW.glfwSetWindowTitle(Main.window, getClass().getName() + " - " + Main.fps + "fps");
+        Main.callEvent(new GameTickEvent());
+    }
+
+    public Colormap getColormap() {
+        return colormap;
+    }
 }
