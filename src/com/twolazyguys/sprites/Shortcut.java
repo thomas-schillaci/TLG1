2c02a56111426dc6b60417498eba32b0828d2bb7
aymericvarasse <taille94Crayon>
2019-06-11 05:50:49 +0200

    Shortcut 2

--- src/com/twolazyguys/sprites/Shortcut.java
+++ src/com/twolazyguys/sprites/Shortcut.java
@@ -1,14 +1,17 @@
 package com.twolazyguys.sprites;
 
 import com.twolazyguys.util.ColorSpritesheet;
+import net.colozz.engine2.events.EventHandler;
+import net.colozz.engine2.events.KeyboardInputEvent;
+import net.colozz.engine2.events.Listener;
 import sun.security.util.Length;
 
 import java.lang.reflect.WildcardType;
 
-public class Shortcut extends Sprite {
+public class Shortcut extends Sprite implements Listener {
 
     private Sprite[] sprites;
-    private static int LENGTH = 90, WIDTH = 65;
+    private static int LENGTH = 90, WIDTH = 60;
     private static int NUMBER_OF_ROWS = 4;
     private static Text[] display = new Text[NUMBER_OF_ROWS];
 
@@ -25,7 +28,7 @@ public class Shortcut extends Sprite {
 
 
     public Shortcut() {
-        super(225, 90, genColors());
+        super(225, 100, genColors());
     }
 
     private static float[][] genColors() {
@@ -34,7 +37,7 @@ public class Shortcut extends Sprite {
         for (int i = 0; i < LENGTH; i++) {
             for (int j = 0; j < WIDTH; j++) {
                 if (i == 0 || i == LENGTH - 1 || j == 0 || j == WIDTH - 1)
-                    res[i][j] = 0.1f;
+                    res[i][j] = 0.075f;
             }
         }
 
@@ -46,6 +49,8 @@ public class Shortcut extends Sprite {
 
         return res;
     }
+
+
 
 
 }
