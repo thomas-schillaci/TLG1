3e11f3c3f0bff9d933a04d11e963c69789a86ca2
aymericvarasse <taille94Crayon>
2019-06-11 03:44:47 +0200

    Shortcut 2

--- src/com/twolazyguys/sprites/Shortcut.java
+++ src/com/twolazyguys/sprites/Shortcut.java
@@ -15,21 +15,17 @@ public class Shortcut extends Sprite {
     private final static int TEXT_OFFSET = 2;
     private final static int LINE_HEIGHT = Text.getLetterSizeY() + TEXT_OFFSET;
 
-    //private static ColorSpritesheet sheet = new ColorSpritesheet(1, 27, "abc");
-
 
     static {
-        display[0] = new Text(TEXT_OFFSET, 1 * LINE_HEIGHT + TEXT_OFFSET, "4. Attack 4");
-        display[1] = new Text(TEXT_OFFSET, 2 * LINE_HEIGHT + TEXT_OFFSET, "3. Attack 3");
-        display[2] = new Text(TEXT_OFFSET, 3 * LINE_HEIGHT + TEXT_OFFSET, "2. Attack 2");
-        display[3] = new Text(TEXT_OFFSET, 4 * LINE_HEIGHT + TEXT_OFFSET, "1. Attack 1");
+        display[0] = new Text(TEXT_OFFSET, 1 * LINE_HEIGHT + TEXT_OFFSET, "4. ");
+        display[1] = new Text(TEXT_OFFSET, 2 * LINE_HEIGHT + TEXT_OFFSET, "3. ");
+        display[2] = new Text(TEXT_OFFSET, 3 * LINE_HEIGHT + TEXT_OFFSET, "2. ");
+        display[3] = new Text(TEXT_OFFSET, 4 * LINE_HEIGHT + TEXT_OFFSET, "1. ");
     }
 
 
     public Shortcut() {
         super(225, 90, genColors());
-        //sprites = new Sprite[]{sheet.getSprite(0, 0), sheet.getSprite(0,1)};
-
     }
 
     private static float[][] genColors() {
@@ -42,20 +38,11 @@ public class Shortcut extends Sprite {
             }
         }
 
-
         for (Text text : display) {
             for (int x = 0; x < text.getColors().length; x++)
                 for (int y = 0; y < text.getColors()[0].length; y++)
                     res[x + text.getX()][y + text.getY()] = text.getColors()[x][y];
         }
-
-
-
-
-
-
-
-
 
         return res;
     }
