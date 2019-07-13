58ab61bddf26dcd98585231d4954e8a529ec62b1
aymericvarasse <taille94Crayon>
2019-07-14 01:47:51 +0200

    Shortcut update

--- src/com/twolazyguys/sprites/Shortcut.java
+++ src/com/twolazyguys/sprites/Shortcut.java
@@ -11,6 +11,7 @@ import java.lang.reflect.WildcardType;
 
 public class Shortcut extends Sprite implements Listener {
 
+    private Sprite[] sprites;
     private static int LENGTH = 90, WIDTH = 60;
     private static int NUMBER_OF_ROWS = 4;
     private static Text[] display = new Text[NUMBER_OF_ROWS];
@@ -20,10 +21,7 @@ public class Shortcut extends Sprite implements Listener {
 
 
     static {
-        display[0] = new Text(TEXT_OFFSET, 1 * LINE_HEIGHT + TEXT_OFFSET, "4. ");
-        display[1] = new Text(TEXT_OFFSET, 2 * LINE_HEIGHT + TEXT_OFFSET, "3. ");
-        display[2] = new Text(TEXT_OFFSET, 3 * LINE_HEIGHT + TEXT_OFFSET, "2. ");
-        display[3] = new Text(TEXT_OFFSET, 4 * LINE_HEIGHT + TEXT_OFFSET, "1. ");
+        for (int i = 0; i < 4; i++) display[i] = new Text(TEXT_OFFSET, (i + 1) * LINE_HEIGHT + TEXT_OFFSET, String.format("%d. ", 4 - i));
     }
 
 
@@ -50,31 +48,56 @@ public class Shortcut extends Sprite implements Listener {
     }
 
     private void setShortcut(int sc, String name) {
-        if (sc >= 1 && sc <= 4) display[sc].setValue((4 - sc) + name);
+        if (sc >= 0 && sc <= 3) display[sc].setValue((4 - sc) + "." + name);
+        setColors(genColors());
+    }
+
+    private void resetShortcut(int sc) {
+        if (sc >= 0 && sc <= 3) display[sc].setValue((4 - sc) + "");
         setColors(genColors());
     }
 
     @EventHandler(EventHandler.Priority.LOWEST)
     public void onCommandEvent(CommandEvent event) {
-        if (!event.isCanceled()) {
+        String formatted = event.getCommand().toLowerCase();
 
-            String formatted = event.getCommand().toLowerCase();
-
-            if (formatted.equals("shortcut")) {
+        if (formatted.equals("bind")) {
+            if (!event.isCanceled()) {
                 event.setCanceled(true);
+
                 if (event.getArgs().length == 0) event.setOutput("Which shortcut?");
                 else {
                     String sc = event.getArgs()[0];
                     String name = event.getArgs()[1];
 
                     switch (sc) {
-                        case "1": setShortcut(3, "." + name); break;
-                        case "2": setShortcut(2, "." + name); break;
-                        case "3": setShortcut(1, "." + name); break;
-                        case "4": setShortcut(0, "." + name); break;
+                        case "1": setShortcut(3, name); break;
+                        case "2": setShortcut(2, name); break;
+                        case "3": setShortcut(1, name); break;
+                        case "4": setShortcut(0, name); break;
                         default: event.setOutput("Only 4 shortcuts allowed"); break;
                     }
 
+                }
+            }
+        }
+
+        else if (formatted.equals("unbind")) {
+            if (!event.isCanceled()) {
+                event.setCanceled(true);
+
+                if (event.getArgs().length == 0) event.setOutput("Which shortcut?");
+                else {
+
+                    String sc = event.getArgs()[0];
+
+                    switch (sc) {
+                        case "1": resetShortcut(3); break;
+                        case "2": resetShortcut(2); break;
+                        case "3": resetShortcut(1); break;
+                        case "4": resetShortcut(0); break;
+                        default: event.setOutput("There are only 4 shortcuts..."); break;
+                    }
                 }
             }
         }
